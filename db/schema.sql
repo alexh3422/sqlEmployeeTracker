d29bbf758819088712e73e879e4504ec63e55fec
Alex Hall <alexhallinthecloud@icloud.com>
2023-02-02 23:22:31 -0800

    updates

--- db/schema.sql
+++ db/schema.sql
@@ -49,6 +49,17 @@ VALUES
 
 
 
+INSERT INTO employees (first_name, last_name, role_id)
+VALUES 
+('Steve', 'Jobs', 5),
+('Roger', 'Rabbit', 4),
+('Mickey', 'Mouse', 3),
+('Donald', 'Duck', 2);
+
+
+
+
+
 
 
 
